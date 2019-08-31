3e961786f18ac28eac895c392cb143a3b5678ce3
Mike Harley <mharley12345@gmail.com>
2019-08-30 20:22:22 -0700

    starts sign up component

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+// import {Formik,withFormik,Field,Form} from 'formik'
+// import * as Yup from 'yup'
+
+const SignUp = ({errors,touched,values,props}) =>{
+
+    return(
+        <div>
+>         <h1>Welcome To Keep Truckin</h1>
+
+          <form>
+          <label>User Name</label>
+          <br/>
+     
+                  <input/><br/><br/>
+          <label>Password</label><br/>
+                  <input/>
+
+            
+          </form>
+
+  
+      </div>
+      
+    )
+}
+export default SignUp
